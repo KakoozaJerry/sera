a661603b3f56da812dc7556cd2c08306d73912f1
KakoozaJerry <abrahamkakooza@gmail.com>
2020-04-11 23:43:09 +0300

    try 1

--- src/estimator.py
+++ src/estimator.py
@@ -1,2 +1,16 @@
 def estimator(data):
   return data
+#Challenge 1
+class Impact:
+  def __init__(self, reportedCases):
+    self.currentlyInfected = reportedCases * 10
+  def infectionsByRequestedTime(self,T):
+    return self.currentlyInfected*(pow(2,(T//3)))
+  pass
+
+class SevereImpact:
+  def __init__(self, reportedCases):
+    self.currentlyInfected = reportedCases * 50
+  def infectionsByRequestedTime(self,T):
+    return self.currentlyInfected*(pow(2,(T//3)))
+  pass
